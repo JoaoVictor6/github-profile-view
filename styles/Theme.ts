505c8cb0d20e1066714587764b273b5acf2fb58f
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-10-18 13:37:47 -0300

    add styled-components theme

---
+++ styles/Theme.ts
@@ -0,0 +1,15 @@
+export type Theme = {
+  background: string
+  textColor1: string
+  textColor2: string
+  blue: string
+  grey: string
+}
+
+export const DarkTheme: Theme = {
+  background: '#1B1F23',
+  blue: '#4969ED',
+  grey: '#6A737D',
+  textColor1: '#FFF',
+  textColor2: '#FAFBFF'
+}
