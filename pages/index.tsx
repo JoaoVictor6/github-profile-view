b5e87b67ab95ee10aa2b02f46b4cad3e97f00882
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-10-18 17:24:44 -0300

    apply eslint rules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,38 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import { FormEvent, useEffect, useRef } from 'react'
-import { Container } from '../styles/Home'
+import Head from "next/head";
+import Link from "next/link";
+import { FormEvent, useRef, } from "react";
+import { Container } from "../styles/Home";
 
 export default function Home() {
-  const ref = useRef<HTMLInputElement>(null)
+  const ref = useRef<HTMLInputElement>(null);
   async function apiHandler(user:string){
-   const request = await fetch(`https://api.github.com/users/${user}/repos`) 
-    const response = await request.json()
-    return response
+    const request = await fetch(`https://api.github.com/users/${user}/repos`); 
+    const response = await request.json();
+    return response;
   }
 
   function submitHandler(event:FormEvent<HTMLFormElement>){
-    event.preventDefault()
-    console.log(apiHandler(ref.current?.value || ""))
+    event.preventDefault();
+    console.log(apiHandler(ref.current?.value || ""));
   }
 
   return (
     <>
-    <Head>
-      <title>Search App</title>
-      <meta name="description" content="Github search app" />
-      <link rel = "icon" type = "image/png" href = "/logo.svg" />
-    </Head>
-    <Container>
-      <div className="logo">
+      <Head>
+        <title>Search App</title>
+        <meta name="description" content="Github search app" />
+        <link rel = "icon" type = "image/png" href = "/logo.svg" />
+      </Head>
+      <Container>
+        <div className="logo">
         Git search
-      </div>
-      <form onSubmit={submitHandler}>
-        <input type="text" placeholder="Search for any github profile" ref={ref}/>
-        <button type="submit">Search</button>
-      </form>
-      <footer><Link href="/">Developed by João ©</Link></footer>
-    </Container>
+        </div>
+        <form onSubmit={submitHandler}>
+          <input type="text" placeholder="Search for any github profile" ref={ref}/>
+          <button type="submit">Search</button>
+        </form>
+        <footer><Link href="/">Developed by João ©</Link></footer>
+      </Container>
     </>
-  )
+  );
 }
