482744431df41d18fc67c815d34b8f453fe886a0
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-10-20 18:50:15 -0300

    improve reset css

--- styles/globals.ts
+++ styles/globals.ts
@@ -1,8 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-  html,
-  body {
+  * {
     padding: 0;
     margin: 0;
     font-family: 'Poppins', sans-serif
