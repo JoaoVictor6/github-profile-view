ef5a79b8a991ea744cb9e8bd528c1706f2537a2d
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-10-20 20:30:53 -0300

    add redirect for user page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,16 @@
 import Head from "next/head";
 import Link from "next/link";
+import {useRouter} from "next/router";
 import { FormEvent, useRef, } from "react";
 import { Container } from "../styles/Home";
 
 export default function Home() {
   const ref = useRef<HTMLInputElement>(null);
-  async function apiHandler(user:string){
-    const request = await fetch(`https://api.github.com/users/${user}/repos`); 
-    const response = await request.json();
-    return response;
-  }
-
+  const route = useRouter();
   function submitHandler(event:FormEvent<HTMLFormElement>){
     event.preventDefault();
-    console.log(apiHandler(ref.current?.value || ""));
+    const name = ref.current?.value || "" ;
+    route.push(`/user/${name}`);
   }
 
   return (
