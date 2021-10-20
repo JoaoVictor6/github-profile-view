08a93434f2b3575655a2dbb40fd7ec0d97c2e894
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-10-20 17:16:11 -0300

    improve theme config

--- styles/Theme.ts
+++ styles/Theme.ts
@@ -1,15 +1,19 @@
 export type Theme = {
   background: string
-  textColor1: string
-  textColor2: string
+  background2: string
+  white: string
+  white2: string
   blue: string
   grey: string
+  greyText: string
 }
 
 export const DarkTheme: Theme = {
   background: "#1B1F23",
+  background2: "#1F2429",
   blue: "#4969ED",
   grey: "#6A737D",
-  textColor1: "#FFF",
-  textColor2: "#FAFBFF"
+  white: "#FFF",
+  white2: "#FAFBFF",
+  greyText: "#6A737D",
 };
