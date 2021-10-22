4178d0558c09f0385690d4ce4653c41852d33bb9
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-10-22 19:16:51 -0300

    add new element

--- styles/user/style.ts
+++ styles/user/style.ts
@@ -3,4 +3,13 @@ import styled from "styled-components";
 export const Container = styled.div`
   min-height: 100vh;
   background: ${props => props.theme.background};
+  display: flex;
+  padding-top: 32px;
+  justify-content: center;
+
+  .repos {
+    display: inline-flex;
+    flex-direction: column;
+    gap: 16px;
+  }
 `;
