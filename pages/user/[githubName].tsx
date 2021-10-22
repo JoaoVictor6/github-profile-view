051b3a9952afffefae16385ea5a79cb5f37f1267
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-10-22 19:17:02 -0300

    implement card component

--- pages/user/[githubName].tsx
+++ pages/user/[githubName].tsx
@@ -1,7 +1,10 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Menu from "../../src/components/Menu";
 import { Container } from "../../styles/user/style";
+import Card from "../../src/components/Card";
+
 interface UserInfoProps {
   avatar_url: string
   html_url: string
@@ -12,10 +15,20 @@ interface UserInfoProps {
   bio: string
 }
 
+interface UserRepoProps {
+  name: string
+  html_url: string
+  fork: boolean
+  language: string
+  stargazers_count: number
+  description: string
+}
+
 export default function User(){
   const router = useRouter();
   const {githubName} = router.query;
   const [userInfo, setUserInfo] = useState({} as UserInfoProps);
+  const [userRepo, setUserRepo] = useState<UserRepoProps[]>([] as UserRepoProps[]);
   
   useEffect(() => {
     async function apiUserInfo(user:string){
@@ -28,9 +41,21 @@ export default function User(){
       .then(res => {
         setUserInfo({...res as UserInfoProps});
       });
-  }, []);
+
+    (async () => {
+      const repoResponse = await fetch(`https://api.github.com/users/${githubName}/repos`);
+      repoResponse.json().then(res => {
+        setUserRepo(res);  
+      });
+      
+    })();
+  }, [githubName]);
   return(
     <>
+      <Head>
+        <title>{userInfo.name} - Github profile</title>
+        <meta name="description" content="Github user profile" />
+      </Head>
       <Menu
         avatar_url={userInfo.avatar_url}
         bio={userInfo.bio}
@@ -41,6 +66,19 @@ export default function User(){
         name={userInfo.name}
       />
       <Container>
+        <div className="repos">
+          {userRepo?.map((repoInfo, index) => (
+            <Card 
+              key={`repo_id${index}`}
+              name={repoInfo.name}
+              description={repoInfo.description}
+              fork={repoInfo.fork}
+              html_url={repoInfo.html_url}
+              language={repoInfo.language}
+              stargazers_count={repoInfo.stargazers_count}
+            />
+          ))}
+        </div>
       </Container>
     </>
   );
