821e51cf7cdec06023942f551dd73f4a94a1ee3e
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-10-29 17:37:05 -0300

    add functionality in FAB

--- src/components/FabScrollTop/index.tsx
+++ src/components/FabScrollTop/index.tsx
@@ -3,11 +3,17 @@ import { Fab } from "./style";
 
 export default function FabScrollTop(){
   const {scrollTop} = UseScrollTop();
+  function scrollTopHandler(){
+    window.scroll(0,0);
+  }
   console.log(scrollTop);
   return(
-    <Fab style={{
-      transform: scrollTop > 100 ? "translateX(0)" : "translateX(100px)"
-    }}>
+    <Fab
+      onClick={scrollTopHandler} 
+      style={{
+        transform: scrollTop > 100 ? "translateX(0)" : "translateX(100px)"
+      }}
+    >
       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none" viewBox="0 0 32 32">
         <g filter="url(#filter0_i_159:5)">
           <path fill="#fff" fillRule="evenodd" d="M8.468 15.531a1.6 1.6 0 010-2.262l6.4-6.4a1.6 1.6 0 012.263 0l6.4 6.4a1.6 1.6 0 01-2.262 2.262l-3.67-3.669V24a1.6 1.6 0 11-3.2 0V11.862l-3.668 3.67a1.6 1.6 0 01-2.263 0z" clipRule="evenodd"/>
