75f77c46a5b44d151a27c5cda717dd8e89ba17aa
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-10-29 17:36:16 -0300

    add smooth effect in elements

--- styles/globals.ts
+++ styles/globals.ts
@@ -1,7 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-  * {
+
+* {
+    scroll-behavior: smooth;
     padding: 0;
     margin: 0;
     font-family: 'Poppins', sans-serif
