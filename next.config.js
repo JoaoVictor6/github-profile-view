1bb8ea1d746bfdf7fe2eeffd25ff58df988cd895
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-02-05 19:19:17 -0300

    add ssg

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/" },
+      "/404": { page: "/404" },
+    };
+  },
 };
