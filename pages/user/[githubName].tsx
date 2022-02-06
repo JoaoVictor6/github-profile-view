b261bea9e6e99453e7511b759f7a3bce46068215
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-02-06 16:23:31 -0300

    add noindex

--- pages/user/[githubName].tsx
+++ pages/user/[githubName].tsx
@@ -107,6 +107,7 @@ function User({error, repo, userInfo: userData}: InferGetServerSidePropsType<Get
           description="Maybe this user does not exist"
           imgUrl="https://i.ibb.co/TqhVm8R/User-not-found-thumb.png"
           url="/404"
+          noindex
         />
       )}
     </>
